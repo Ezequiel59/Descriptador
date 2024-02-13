2efa808618d15719ce3a74e1729020e09c631ea7
Chequel197 <chequel197@gmail.com>
2024-02-13 08:17:43 -0600

    cambias en CSS  v.6

---
+++ app.js
@@ -0,0 +1,66 @@
+// codigo para codificar
+// si hace click comienza la encriptacion del 
+
+// let codigo = ""
+// let encriptado = codigo.split("");
+// let desifrado;
+// console.log(encriptado)
+
+//se encapsula en una funcion para que sea llamada
+// cuando se haga clink en el boton
+function encriptar(){
+    let codigo = document.getElementById("textoIncriptar").value;
+    let encriptado = codigo.split("");
+    let parrafo = document.getElementById("textoEncriptadoResultado")
+    // let desifrado;
+    // console.log(encriptado)
+
+    for (let index = 0; index < encriptado.length; index++) { //comprueba si hay valores parecido dentro del array
+        if (encriptado[index] == "a") {
+             encriptado[index] = "$"
+        } else if(encriptado[index] == "e"){
+             encriptado[index] = "%"
+        } else if(encriptado[index] == "i"){
+             encriptado[index] = "&"
+        } else if(encriptado[index] == "o"){
+             encriptado[index] = "/"
+        } else if(encriptado[index] == "u"){
+             encriptado[index] = "("
+        }
+     }
+
+    if(document.getElementById("textoIncriptar").value != 0){
+       parrafo.innerText = encriptado.join("")
+    }
+     
+} 
+
+function desencriptar(){
+     let codigo = document.getElementById("textoIncriptar").value;
+     let encriptado = codigo.split("");
+     let parrafo = document.getElementById("textoEncriptadoResultado")
+
+    for (let index = 0; index < encriptado.length; index++) {
+        if (encriptado[index] == "$") {
+             encriptado[index] = "a"
+        } else if(encriptado[index] == "%"){
+             encriptado[index] = "e"
+        } else if(encriptado[index] == "&"){
+             encriptado[index] = "i"
+        } else if(encriptado[index] == "/"){
+             encriptado[index] = "o"
+        } else if(encriptado[index] == "("){
+             encriptado[index] = "u"
+        }
+     } 
+     if(document.getElementById("textoIncriptar").value != 0){
+          parrafo.innerText = encriptado.join("")
+       }
+}
+
+
+let parrafo = document.getElementById("desencriptar")
+parrafo.addEventListener("click", desencriptar)
+
+parrafo = document.getElementById("botonEncriptar")
+parrafo.addEventListener("click", encriptar)
